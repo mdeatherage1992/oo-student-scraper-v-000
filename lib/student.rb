7a74c36761324f6afde2e384809bda9c5c555002
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-28 11:58:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,8 +13,8 @@ class Student
 end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student|
-      Self.new(student)
+    students_array.each do |student_hash|
+      Self.new(student_hash)
   end
 end
 
